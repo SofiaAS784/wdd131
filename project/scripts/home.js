7d5ec8864073c74469a2d05f56b965478f8fdbf3
SofiaAS784 <ssuarez2@byupathway.edu>
2025-12-06 16:56:38 -0300

    web page project

---
+++ project/scripts/home.js
@@ -0,0 +1,17 @@
+const menuButton = document.querySelector('#menu');
+const navigation = document.querySelector('ul');
+
+menuButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    menuButton.classList.toggle('open');
+});
+
+const animatedText = document.querySelector('#animation');
+let textIndex = 0;
+function changeText() {
+    const hills = ['Sierra de los Padres', 'Cerro Ventana', 'Sierra de los Difuntos', 'Sierra Barrosa', 'Cerro Tres Picos'];
+    animatedText.textContent = hills[textIndex];
+    textIndex = (textIndex + 1) % hills.length;
+}
+
+setInterval(changeText, 1500);
